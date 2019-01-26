91b79f6a5ea30d279447d7222648246984f13225
PrathamRawat <prathamrawat815@gmail.com>
2019-01-26 14:10:22 -0500

    Add batch file to repo

---
+++ FRC.bat
@@ -0,0 +1,16 @@
+@echo
+
+cd "C:\Users\Public\frc2019\vscode\"
+start Code.exe
+
+cd "C:\Program Files (x86)\FRC Driver Station\"
+start DriverStation.exe
+
+cd "C:\Users\Public\frc2019\tools\"
+start shuffleboard.vbs
+
+cd "C:\Program Files\Git\"
+start git-bash.exe --cd-to-home
+
+exit
+
